399f0494ceea278cbf09d20c22c85a10743f9b60
AlekseyChurilov <118053184+AlekseyChurilov@users.noreply.github.com>
2024-01-15 01:25:34 +0300

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/m6129/UrFU_2022_python/main/Dolganov/2_%D1%81%D0%B5%D0%BC>
+df = pd.read_csv('https://raw.githubusercontent.com/m6129/UrFU_2022_python/main/Dolganov/2_%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/train.csv')
 df.to_csv('/home/data1/lab1/df.csv')
 print('data_creation исполнен')
