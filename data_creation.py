9f01408cc9adf858ef5a9cb6a1750e864f8c0b80
AlekseyChurilov <118053184+AlekseyChurilov@users.noreply.github.com>
2024-01-15 01:24:27 +0300

    Create data_creation.py

---
+++ data_creation.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/m6129/UrFU_2022_python/main/Dolganov/2_%D1%81%D0%B5%D0%BC>
+df.to_csv('/home/data1/lab1/df.csv')
+print('data_creation исполнен')
